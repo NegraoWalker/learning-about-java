ddc1dcd0402bf85787bf57d4120e68f96946b6d6
walkerNegrão <walker.esteves.negrao@gmail.com>
2025-02-23 21:49:53 -0300

    Novo contéudo adicionado

---
+++ src/conteudo1/SaidaDeDados.java
@@ -0,0 +1,14 @@
+package conteudo1;
+
+public class SaidaDeDados {
+    public static void main(String[] args) {
+        String name = "Walker";
+        int age = 32;
+        double salary = 3150.453;
+        boolean male = true;
+
+        System.out.print(name);
+        System.out.println(age);
+        System.out.printf("Meu nome é %s, tenho %d anos. Meu salário é R$%.2f, sou do sexo masculino: %b", name, age, salary, male);
+    }
+}
---
+++ src/conteudo1/Variaveis.java
@@ -0,0 +1,20 @@
+package conteudo1;
+
+public class Variaveis {
+    public static void main(String[] args) {
+        //Comentário de uma única linha
+        /*
+           *Comentário de múltiplas linhas
+        */
+
+        /*
+            Template para criar variáveis: tipo-de-dado nome-da-variável = valor;
+        */
+
+        String nome = "Walker";
+        int idade = 32;
+        double salarioAtual = 3120.465;
+        
+
+    }
+}
