aa02df31eab4329e7653a34ee31206364f1f6e4a
walkerNegrão <walker.esteves.negrao@gmail.com>
2025-02-25 20:36:21 -0300

    Atualização - ConversaoEntreTiposPrimitivos

--- src/conteudo1/ConversaoEntreTiposPrimitivos.java
+++ src/conteudo1/ConversaoEntreTiposPrimitivos.java
@@ -2,6 +2,24 @@ package conteudo1;
 
 public class ConversaoEntreTiposPrimitivos {
     public static void main(String[] args) {
+        byte var1 = 25;
+        short var2 = 1000;
+        int var3 = 2000000;
+        long var4 = 7800000000L;
+        float var5 = 1.756f;
+        double var6 = 68.833;
+        char var7 = 'A';
 
+        short conversaoDeByteParaShort = var1;
+        System.out.println(conversaoDeByteParaShort);
+
+        short conversaoIntParaShort = (short) var3;
+        System.out.println(conversaoIntParaShort);
+
+        long conversaoFloatParaLong = (long) var5;
+        System.out.println(conversaoFloatParaLong);
+
+        double conversaLongParaDouble = (double) var4;
+        System.out.println(conversaLongParaDouble);
     }
 }
