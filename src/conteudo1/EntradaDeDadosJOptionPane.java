4cdcb9a4702d9276dfcf256371ba497732e79969
Walker Negrao <1882452@tcs.com>
2025-02-27 13:54:24 -0300

    Atualização - EntradaDeDados

---
+++ src/conteudo1/EntradaDeDadosJOptionPane.java
@@ -0,0 +1,16 @@
+package conteudo1;
+
+import javax.swing.*;
+import java.util.Locale;
+
+public class EntradaDeDadosJOptionPane {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+
+        String name = JOptionPane.showInputDialog("Digite seu nome: ");
+        String age = JOptionPane.showInputDialog("Digite sua idade: ");
+        int convAge = Integer.parseInt(age);
+
+        System.out.printf("Meu nome é %s, tenho %d anos.", name, convAge);
+    }
+}
