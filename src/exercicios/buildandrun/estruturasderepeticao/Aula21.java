96f72e572e305b19cc29aa753b15052233fdd6cd
Walker Negrao <1882452@tcs.com>
2025-03-19 16:13:55 -0300

    Atualização

---
+++ src/exercicios/buildandrun/estruturasderepeticao/Aula21.java
@@ -0,0 +1,12 @@
+package exercicios.buildandrun.estruturasderepeticao;
+
+import java.util.Scanner;
+
+public class Aula21 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        scanner.close();
+    }
+}
