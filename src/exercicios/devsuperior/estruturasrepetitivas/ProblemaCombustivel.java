77c06f31540cf8e237dafb732e48c9a5a900b196
walkerNegrão <walker.esteves.negrao@gmail.com>
2025-03-23 15:40:54 -0300

    Atualização

--- src/exercicios/devsuperior/estruturasrepetitivas/ProblemaCombustivel.java
+++ src/exercicios/devsuperior/estruturasrepetitivas/ProblemaCombustivel.java
@@ -1,7 +1,32 @@
 package exercicios.devsuperior.estruturasrepetitivas;
 
+import java.util.Scanner;
+
 public class ProblemaCombustivel {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int entrada;
+        int alcool = 0, gasolina = 0, diesel = 0;
 
+
+        do {
+            System.out.println("Informe um código [1 - Álcool; 2 - Gasolina; 3 - Diesel; 4 - Finalizar]: ");
+            entrada = scanner.nextInt();
+            switch (entrada) {
+                case 1:
+                    alcool++;
+                    break;
+                case 2:
+                    gasolina++;
+                    break;
+                case 3:
+                    diesel++;
+                    break;
+            }
+        } while (entrada != 4);
+        System.out.println("Muito obrigado!");
+        System.out.printf("ÁLCOOL: %d\nGASOLINA: %d\nDIESEL: %d\n", alcool, gasolina, diesel);
+
+        scanner.close();
     }
 }
--- src/exercicios/devsuperior/estruturasrepetitivas/ProblemaQuadrante.java
+++ src/exercicios/devsuperior/estruturasrepetitivas/ProblemaQuadrante.java
@@ -1,7 +1,26 @@
 package exercicios.devsuperior.estruturasrepetitivas;
 
+import java.util.Scanner;
+
 public class ProblemaQuadrante {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int x, y;
+        do {
+            System.out.println("Digite as coordenadas x e y: ");
+            x = scanner.nextInt();
+            y = scanner.nextInt();
+            if (x > 0 && y > 0) {
+                System.out.println("Q1");
+            } else if (x < 0 && y > 0) {
+                System.out.println("Q2");
+            } else if (x < 0 && y < 0) {
+                System.out.println("Q3");
+            } else if (x > 0 && y < 0) {
+                System.out.println("Q4");
+            }
+        }
+        while (x != 0 && y != 0);
+        scanner.close();
     }
 }
