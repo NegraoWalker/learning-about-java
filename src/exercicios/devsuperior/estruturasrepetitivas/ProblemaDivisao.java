f2dd5b6487000e2acf5c293eac0ff19ab4402ab0
Walker Negrao <1882452@tcs.com>
2025-03-25 09:58:33 -0300

    Atualização

--- src/exercicios/devsuperior/estruturasrepetitivas/ProblemaDivisao.java
+++ src/exercicios/devsuperior/estruturasrepetitivas/ProblemaDivisao.java
@@ -1,7 +1,32 @@
 package exercicios.devsuperior.estruturasrepetitivas;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ProblemaDivisao {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        int numerador, denominador;
+        double divisao;
 
+        System.out.println("Quantos casos você vai digitar: ");
+        n = scanner.nextInt();
+
+        for (int i = 0; i < n; i++) {
+
+            System.out.println("Entre com o numerador: ");
+            numerador = scanner.nextInt();
+            System.out.println("Entre com o denominador: ");
+            denominador = scanner.nextInt();
+            if (denominador != 0) {
+                divisao = (double) numerador / denominador;
+                System.out.printf("Resultado da divisão: %.2f\n", divisao);
+            } else {
+                System.out.println("Divisão impossível!");
+            }
+        }
+        scanner.close();
     }
 }
--- src/exercicios/devsuperior/estruturasrepetitivas/ProblemaFatorial.java
+++ src/exercicios/devsuperior/estruturasrepetitivas/ProblemaFatorial.java
@@ -1,7 +1,25 @@
 package exercicios.devsuperior.estruturasrepetitivas;
 
+import java.util.Scanner;
+
 public class ProblemaFatorial {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        int fatorial = 1;
 
+        System.out.println("Informe o número para o cálculo do fatorial: ");
+        n = scanner.nextInt();
+
+        if (n < 15) {
+            for (int i = n; i >= 1 ; i--) {
+                fatorial *= i;
+            }
+        } else {
+            System.out.println("Número fora do range!");
+        }
+
+        System.out.println("FATORIAL: " + fatorial);
+        scanner.close();
     }
 }
