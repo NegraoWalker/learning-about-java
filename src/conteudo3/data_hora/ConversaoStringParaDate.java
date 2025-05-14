afdc39e7f946d49a6dcf07bdcd08fc161e20e77c
Walker Negrao <1882452@tcs.com>
2025-05-14 10:30:36 -0300

    Atualização

--- src/conteudo3/data_hora/ConversaoStringParaDate.java
+++ src/conteudo3/data_hora/ConversaoStringParaDate.java
@@ -6,13 +6,13 @@ import java.time.format.DateTimeFormatter;
 public class ConversaoStringParaDate {
     public static void main(String[] args) {
         //CONVERSÃO:
-        LocalDate dataLocal1 = LocalDate.parse("2025-05-16"); //Conversão de um String para uma data local
+        LocalDate dataLocal1 = LocalDate.parse("2025-05-16"); //Conversão de uma String para uma data local
         System.out.println("Data Local 1 [Conversão de String para data - Formato no padrão ISO 8601]: " + dataLocal1);
 
-        LocalTime horaLocal1 = LocalTime.parse("21:45:41.561900"); //Conversão de um String para uma hora
+        LocalTime horaLocal1 = LocalTime.parse("21:45:41.561900"); //Conversão de uma String para uma hora
         System.out.println("Hora Local 1 [Conversão de String para hora - Formato no padrão ISO 8601]: " + horaLocal1);
 
-        LocalDateTime dataEHoraLocal1 = LocalDateTime.parse("2025-05-14T13:30:42.561900"); //Conversão de um String para uma data e hora local
+        LocalDateTime dataEHoraLocal1 = LocalDateTime.parse("2025-05-14T13:30:42.561900"); //Conversão de uma String para uma data e hora local
         System.out.println("Data e Hora Local 1 [Conversão de String para data e hora - Formato no padrão ISO 8601]: " + dataEHoraLocal1);
 
         Instant dataEHoraGlobal1 = Instant.parse("2025-05-08T23:00:12.116498600Z");
