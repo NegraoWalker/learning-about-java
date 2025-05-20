acfbdc6508b93bca4d6b21642f7ccd4eac9ebfa9
Walker Negrao <1882452@tcs.com>
2025-05-20 15:39:13 -0300

    Atualização

---
+++ src/conteudo3/composicao/devsuperior/exemplo1/Main.java
@@ -0,0 +1,7 @@
+package conteudo3.composicao.devsuperior.exemplo1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/conteudo3/composicao/devsuperior/exemplo1/WorkerLevel.java
@@ -0,0 +1,7 @@
+package conteudo3.composicao.devsuperior.exemplo1;
+
+public enum WorkerLevel {
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR
+}
