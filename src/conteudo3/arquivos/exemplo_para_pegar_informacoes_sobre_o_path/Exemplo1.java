c7206d29e58b01d11a047fc4d233fb4d4405a2e4
walkerNegrão <walker.esteves.negrao@gmail.com>
2025-06-20 09:13:15 -0300

    Atualizaç~Ãão

---
+++ src/conteudo3/arquivos/exemplo_para_pegar_informacoes_sobre_o_path/Exemplo1.java
@@ -0,0 +1,20 @@
+package conteudo3.arquivos.exemplo_para_pegar_informacoes_sobre_o_path;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Exemplo1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Entre com o path do arquivo: ");
+        String strPath = scanner.nextLine();
+
+        File file = new File(strPath);
+
+        System.out.println("Informações: ");
+        System.out.println("Nome do arquivo: " + file.getName());
+        System.out.println("Nome da pasta que o arquivo está armazenado: " + file.getParent());
+        scanner.close();
+    }
+}
